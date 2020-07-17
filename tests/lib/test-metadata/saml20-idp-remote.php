dd47df5ce07fedb32669bc5f147e653ed0088e21
Guy Halse <guy@tenet.ac.za>
2020-07-17 14:15:41 +0200

    First pass at some unit tests

---
+++ tests/lib/test-metadata/saml20-idp-remote.php
@@ -0,0 +1,27 @@
+<?php
+$metadata['https://idp01.example.org'] = [
+    'name' => ['en' => 'IdP 01'],
+    'discopower.weight' => 1,
+    'tags' => ['A'],
+];
+$metadata['https://idp02.example.org'] = [
+    'name' => ['en' => 'IdP 02'],
+    'tags' => ['A'],
+];
+$metadata['https://idp03.example.org'] = [
+    'name' => ['en' => 'IdP 03'],
+    'discopower.weight' => 100,
+    'tags' => ['A'],
+];
+$metadata['https://idp04.example.org'] = [
+    'name' => ['en' => 'IdP 04'],
+    'tags' => ['A', 'B'],
+];
+$metadata['https://idp05.example.org'] = [
+    'tags' => ['B'],
+];
+$metadata['https://idp06.example.org'] = [
+    'name' => ['en' => 'IdP 06'],
+    'tags' => ['B'],
+];
+
