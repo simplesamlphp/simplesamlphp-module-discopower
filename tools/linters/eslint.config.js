f685b6178969be008e39827c821c14c4e82bb8e6
Tim van Dijen <tvdijen@gmail.com>
2025-05-12 23:31:50 +0200

    Replace super-linter with a reusable workflow

--- tools/linters/eslint.config.js
+++ tools/linters/eslint.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("eslint/config");
 
 module.exports = defineConfig([
     {
-        ignores: ["!/tools/linters/.eslint.config.js", "!/tools/linters/.stylelintrc.json", "!/public/assets/components/jquery-ui.min.js"],
+        ignores: ["!/tools/linters/.eslint.config.js", "!/tools/linters/.stylelintrc.json", "/public/assets/components/jquery-ui.min.js"],
         languageOptions: {
             ecmaVersion: 2015,
             sourceType: "module"
